e17dcc474d787c3c27c3831e94160013271218f0
George Charitos <114592123+georgexar@users.noreply.github.com>
2025-05-22 14:20:53 +0300

    loaded

--- FallenKingdom/Assets/_bScripts/UIscripts/Store/StoreItemSoundFx.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class StoreItemSoundFx : MonoBehaviour
-{
-    public void PlaySoundBtn()
-    {
-        GameManager.Instance.SoundsFxManager.PlaySoundAtIndex(0);
-    }
-}
